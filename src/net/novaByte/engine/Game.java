55aadda8dfd14ad2d54e41e7557961ef91ea9f6b
navx2810 <navx2810@gmail.com>
2014-07-28 00:57:37 -0400

    Revert "7-27-14 vB"
    This reverts commit 4f1b82cc69afb501a9dfa49790295ff3e00de9ca.

--- src/net/novaByte/engine/Game.java
+++ src/net/novaByte/engine/Game.java
@@ -5,27 +5,16 @@ import org.lwjgl.input.Keyboard;
 public class Game 
 {
 	private Mesh mesh;
-	private Shader shader;
-	private Transform transform;
 	
 	public Game() 
 	{
 		mesh = new Mesh();
-		shader = new Shader();
 		
 		Vertex[] data = new Vertex[] { 	new Vertex(new Vector3f(-1, -1, 0)),
 										new Vertex(new Vector3f(0, 1, 0)),
 										new Vertex(new Vector3f(1, -1, 0)) };
 		
 		mesh.addVertices(data);
-		
-		transform = new Transform();
-		
-		shader.addVertexShader(ResourceLoader.loadShader("basicVertex.vert"));
-		shader.addFragmentShader(ResourceLoader.loadShader("basicFragment.frag"));
-		shader.compileShader();
-		
-		shader.addUniform("transform");
 	}
 	
 	public void input()
@@ -42,26 +31,13 @@ public class Game
 			
 	}
 	
-	float temp = 0.0f;
-	
 	public void update()
 	{
-//		if(temp <= 3f)
-			temp += Time.getDelta(); 	// TODO: This is a simple tween--thing. Build a tween engine off of this
-//		System.out.println(temp);
-			
-		float sinTemp = (float)(Math.sin(temp));
 		
-		transform.setTranslation(sinTemp, 0, 0);
-		transform.setRotation(0,0, sinTemp * 180);
-		transform.setScale( sinTemp, sinTemp, sinTemp );
-			
 	}
 	
 	public void render()
 	{
-		shader.bind();
-		shader.setUniform("transform", transform.getTransformation());
 		mesh.draw();
 	}
 	
